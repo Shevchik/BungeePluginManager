0ce150b42babf5973d9327e377d68662a78cdd62
Shevchik <Shev4ik.den@gmail.com>
2020-04-27 20:15:46 +0300

    Inject custom event bus on load
    Injecting custom event bus on enable could result in lossing some
    already registered listeners

--- src/bungeepluginmanager/BungeePluginManager.java
+++ src/bungeepluginmanager/BungeePluginManager.java
@@ -1,14 +1,24 @@
 package bungeepluginmanager;
 
+import java.util.logging.Level;
+
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeePluginManager extends Plugin {
 
 	@Override
+	public void onLoad() {
+		try {
+			ReflectionUtils.setFieldValue(ProxyServer.getInstance().getPluginManager(), "eventBus", new ModifiedPluginEventBus());
+		} catch (IllegalAccessException | NoSuchFieldException e) {
+			getLogger().log(Level.SEVERE, "Unable to inject modified command bus, completing plugin async intents won't work", e);
+		}
+	}
+
+	@Override
 	public void onEnable() {
-		ProxyServer.getInstance().getPluginManager().registerCommand(this, new Commands());
-		ReflectionUtils.setFieldValue(ProxyServer.getInstance().getPluginManager(), "eventBus", new ModifiedPluginEventBus());
+		getProxy().getPluginManager().registerCommand(this, new Commands());
 	}
 
 }
