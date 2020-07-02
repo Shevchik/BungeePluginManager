fbbd628defd203c2ea5a435c79161b361498b307
itsTyrion <me@itstyrion.de>
2020-07-02 16:22:49 +0200

    Added full command tab completion Fixed bugs with turkish system locale Chopped some very long lines down Slight clean ups

--- src/bungeepluginmanager/ReflectionUtils.java
+++ src/bungeepluginmanager/ReflectionUtils.java
@@ -13,48 +13,48 @@ public class ReflectionUtils {
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T> T getFieldValue(Object obj, String fieldname) throws IllegalAccessException, NoSuchFieldException {
+	public static <T> T getFieldValue(Object obj, String fieldName) throws IllegalAccessException, NoSuchFieldException {
 		Class<?> clazz = obj.getClass();
 		do {
 			for (Field field : clazz.getDeclaredFields()) {
-				if (field.getName().equals(fieldname)) {
-					return (T) setAccessible(clazz.getDeclaredField(fieldname)).get(obj);
+				if (field.getName().equals(fieldName)) {
+					return (T) setAccessible(clazz.getDeclaredField(fieldName)).get(obj);
 				}
 			}
 		} while ((clazz = clazz.getSuperclass()) != null);
-		throw new NoSuchFieldException("Can't find field " + fieldname);
+		throw new NoSuchFieldException("Can't find field " + fieldName);
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T> T getStaticFieldValue(Class<?> clazz, String fieldname) throws IllegalAccessException, NoSuchFieldException {
+	public static <T> T getStaticFieldValue(Class<?> clazz, String fieldName) throws IllegalAccessException, NoSuchFieldException {
 		do {
 			for (Field field : clazz.getDeclaredFields()) {
-				if (field.getName().equals(fieldname)) {
-					return (T) setAccessible(clazz.getDeclaredField(fieldname)).get(null);
+				if (field.getName().equals(fieldName)) {
+					return (T) setAccessible(clazz.getDeclaredField(fieldName)).get(null);
 				}
 			}
 		} while ((clazz = clazz.getSuperclass()) != null);
-		throw new NoSuchFieldException("Can't find field " + fieldname);
+		throw new NoSuchFieldException("Can't find field " + fieldName);
 	}
 
-	public static void setFieldValue(Object obj, String fieldname, Object value) throws IllegalAccessException, NoSuchFieldException {
+	public static void setFieldValue(Object obj, String fieldName, Object value) throws IllegalAccessException, NoSuchFieldException {
 		Class<?> clazz = obj.getClass();
 		do {
 			for (Field field : clazz.getDeclaredFields()) {
-				if (field.getName().equals(fieldname)) {
-					setAccessible(clazz.getDeclaredField(fieldname)).set(obj, value);
+				if (field.getName().equals(fieldName)) {
+					setAccessible(clazz.getDeclaredField(fieldName)).set(obj, value);
 					return;
 				}
 			}
 		} while ((clazz = clazz.getSuperclass()) != null);
-		throw new NoSuchFieldException("Can't find field " + fieldname);
+		throw new NoSuchFieldException("Can't find field " + fieldName);
 	}
 
-	public static void invokeMethod(Object obj, String methodname, Object... args) throws IllegalAccessException, InvocationTargetException {
+	public static void invokeMethod(Object obj, String methodName, Object... args) throws IllegalAccessException, InvocationTargetException {
 		Class<?> clazz = obj.getClass();
 		do {
 			for (Method method : clazz.getDeclaredMethods()) {
-				if (method.getName().equals(methodname) && (method.getParameterTypes().length == args.length)) {
+				if (method.getName().equals(methodName) && (method.getParameterTypes().length == args.length)) {
 					setAccessible(method).invoke(obj, args);
 				}
 			}
